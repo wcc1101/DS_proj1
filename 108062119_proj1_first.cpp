33193554e328e8f8aebe5b8afd99ef46fb9cf49b
wcc <davidc1101@gmail.com>
2020-10-03 22:53:28 +0800

    ff

---
+++ 108062119_proj1_first.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
+using namespace std;
+
+int m, n;
+int **ground;
+
+int main()
+{
+    ifstream fin;
+    fin.open("testcase.txt");
+    if (!fin)
+        cout << "fail to input" << endl;
+
+    //fin.get(m); //rows
+    //fin.get(n); //cols
+    ground = new int *[m];
+    for (int i = 0; i < m; i++)
+        ground[i] = new int[n]; //make m*n ground
+}
