08758ad914e663e17fea7a6eaf93f5a5cd0c0281
wcc <davidc1101@gmail.com>
2020-10-13 10:54:44 +0800

    10/13.1(answer using cout)

--- 108062119_proj1_first.cpp
+++ 108062119_proj1_first.cpp
@@ -3,7 +3,6 @@
 #include <map>
 #include <string>
 using namespace std;
-
 int **ground, width, height;
 int nowblock[4][4];
 ifstream fin;
@@ -28,7 +27,6 @@ int sets_pos[19][4][4] =
         {{1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}, {1, 0, 0, 0}},
         {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 1, 1}},
         {{0, 0, 0, 0}, {0, 0, 0, 0}, {1, 1, 0, 0}, {1, 1, 0, 0}}};
-
 map<string, int> sets{
     {"T1", 0},
     {"T2", 1},
@@ -49,7 +47,6 @@ map<string, int> sets{
     {"I1", 16},
     {"I2", 17},
     {"O", 18}};
-
 int **set(void)
 {
     fin.open("testcase.txt");
@@ -66,7 +63,6 @@ int **set(void)
         g[i] = new int[width]{};
     return g;
 }
-
 void display(void)
 {
     for (int i = 4; i < height; i++)
@@ -76,78 +72,62 @@ void display(void)
         cout << endl;
     }
 }
-
 bool collision(int r, int c)
 {
+    if (r == height)
+        return 1;
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
-            if ((nowblock[i][j] && ground[r - 3 + i][c + j]) || r == height)
+            if (nowblock[i][j] && ground[r - 3 + i][c + j])
                 return 1;
     return 0;
 }
-
 int drop(int r, int c)
 {
     while (!collision(r + 1, c))
     {
-        //cout << "now row: " << r + 1 << endl;
         r++;
     }
     return r;
 }
-
 void erase(int r, int c)
 {
-    //cout << r << endl;
     for (int i = 0; i < 4; i++)
         for (int j = 0; j < 4; j++)
             if (nowblock[i][j])
                 ground[r - 3 + i][c + j] = 1;
-    //display();
     int x = 0;
     for (int i = 3; i >= 0; i--)
     {
-        //cout << "x: " << x << endl;
         int flag = 1;
         for (int j = 0; j < width; j++)
             if (!ground[r - 3 + i + x][j])
                 flag = 0;
         if (flag)
         {
-            //cout << "erase: " << r - 3 + i + x << endl;
             for (int j = 0; j < width; j++)
                 ground[r - 3 + i + x][j] = 0;
             for (int o = r - 3 + i + x; o > 3; o--)
                 for (int p = 0; p < width; p++)
                     ground[o][p] = ground[o - 1][p];
-            //display();
             x++;
         }
     }
 }
-
 void start(void)
 {
     string str;
     while (fin >> str)
     {
-        //cout << str << endl;
         if (str == "End")
             break;
         else
         {
             int sp, st;
             fin >> sp >> st;
-            //cout << sp << ' ' << st << endl;
             for (int i = 0; i < 4; i++)
-            {
                 for (int j = 0; j < 4; j++)
-                {
                     nowblock[i][j] = sets_pos[sets[str]][i][j];
-                    //cout << nowblock[i][j];
-                }
-                //cout << endl;
-            }
             int row = 4;
             int col = sp - 1;
             row = drop(row, col);
@@ -157,11 +137,9 @@ void start(void)
         }
     }
 }
-
 int main()
 {
     ground = set();
-    //display();
     start();
     display();
 }
